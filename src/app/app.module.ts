3fbffb9a928682b25ce1134d4b6b1d2d8830f267
HP <nishijayaraj@gmail.com>
2025-08-16 00:09:32 +0530

    App Routing : Added the project with standalone false

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContentProjectionComponent } from './content-projection/content-projection.component';
+
+import { appRoutes } from './app.routes';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
+  declarations: [AppComponent, ContentProjectionComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { ContentProjectionComponent } from './content-projection/content-projection.component';
+import { AppComponent } from './app.component';
+
+export const appRoutes: Routes = [
+  { path: 'cp', component: ContentProjectionComponent },
+];
